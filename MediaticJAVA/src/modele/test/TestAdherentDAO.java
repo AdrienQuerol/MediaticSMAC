52e52e2dc08f35eed607b726138989b337c131b5
Mikael Morin <mikael.morin2@gmail.com>
2016-01-27 17:01:33 +0100

    Ajout test empruntsEnCours (marche pas)

--- MediaticJAVA/src/modele/test/TestAdherentDAO.java
+++ MediaticJAVA/src/modele/test/TestAdherentDAO.java
@@ -2,10 +2,18 @@ package modele.test;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.TreeSet;
 
 import dao.AdherentDAO;
 import dao.Classement;
+import dao.EmpruntDAO;
+import dao.MediaDAO;
 import modele.adherent.Adherent;
+import modele.adherent.Emprunt;
+import modele.media.CD;
+import modele.media.DVD;
+import modele.media.Livre;
+import modele.media.Media;
 
 public class TestAdherentDAO {
 	public static void main (String[] args) {
@@ -38,5 +46,45 @@ public class TestAdherentDAO {
 		a = adhs.get(0);
 		if (!("Peladan".equals(a.getNom()) && "Cécile".equals(a.getPrenom())))
 			throw new RuntimeException("Adhérent retourné invalide: " + a.getPrenom() + " " + a.getNom());
+
+		MediaDAO mediaBD = new MediaDAO();
+		Media titre       = new CD("titre","bobobobo");
+		Media fleursDuMal = new Livre("Les fleurs du mal","baudelaire");
+		Media batman      = new DVD("Batman","Joker");
+		Media starWars    = new DVD("Star Wars","Lucas");
+		Media inExtremis  = new CD("In extremis","Cabrel");
+
+		Emprunt e1 = new Emprunt(pDupont,  titre,       LocalDate.of(2016, 01, 14));		
+		Emprunt e2 = new Emprunt(pDupont,  fleursDuMal, LocalDate.now());
+		Emprunt e3 = new Emprunt(cPeladan, titre,       LocalDate.now());
+		Emprunt e4 = new Emprunt(jMartin,  titre,       LocalDate.now());
+		Emprunt e5 = new Emprunt(pDupont,  batman,      LocalDate.of(2016,  01,  8));
+		
+		mediaBD.save(titre);
+		mediaBD.save(fleursDuMal);
+		mediaBD.save(batman);
+		mediaBD.save(starWars);
+		mediaBD.save(inExtremis);
+		
+		EmpruntDAO empruntBD = new EmpruntDAO();
+		empruntBD.save(e1);
+		empruntBD.save(e2);
+		empruntBD.save(e3);
+		empruntBD.save(e4);
+		empruntBD.save(e5);
+		
+		List<Emprunt> empruntsDupont = AdherentDAO.empruntsEnCours(pDupont);
+		if (empruntsDupont.size() != 3)
+			throw new RuntimeException("Nombre de médias retournés incorrect: "  + empruntsDupont.size());
+		TreeSet<Long> bonsIDEmpruntsDupont, IDEmpruntsDupont;
+		bonsIDEmpruntsDupont = new TreeSet<Long>();
+		bonsIDEmpruntsDupont.add(e1.getID());
+		bonsIDEmpruntsDupont.add(e2.getID());
+		bonsIDEmpruntsDupont.add(e5.getID());
+		IDEmpruntsDupont = new TreeSet<Long>();
+		if (!bonsIDEmpruntsDupont.equals(IDEmpruntsDupont))
+			throw new RuntimeException("Liste d'emprunts renvoyés incorrecte");
+
+		
 	}
 }
